d0fa14dd5839ebdacf14cef15609883de0023b65
Monkey <duyk16@gmail.com>
2019-01-23 21:12:52 +0700

    adding some initial middlewares

--- routes/Exercise.js
+++ routes/Exercise.js
@@ -2,6 +2,12 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => {
+  console.log(process.env);
+  
   res.send(process.env.MONGO_URL)
 })
+route.post('/', (req, res) => {
+
+})
+
 module.exports = route
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -7,6 +9,12 @@ const timeStampRoute = require('./routes/TimeStamp')
 const whoAmI = require('./routes/WhoAmI')
 const exercise = require('./routes/Exercise')
 
+// Initial middlewares
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true }))
+
+// Routers
 app.use('/api/timestamp', timeStampRoute)
 app.use('/api/whoami', whoAmI)
 app.use('/api/exercise', exercise)
