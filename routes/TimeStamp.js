a6ded20c5fa7654f891e380d478dfc84cfdc14e4
Monkey <duyk16@gmail.com>
2019-01-23 17:07:29 +0700

    fix syntax

--- routes/TimeStamp.js
+++ routes/TimeStamp.js
@@ -12,7 +12,7 @@ route.get('/', (req, res) => {
 route.get('/:dateString', (req, res) => {
   let dateString = req.params.dateString
   dateString = dateString == parseInt(dateString) ? parseInt(dateString) : dateString
-  date = new Date(dateString);
+  let date = new Date(dateString);
   if (date == "Invalid Date") {
     return res.status(300).send({
       error: "Invalid Date"
