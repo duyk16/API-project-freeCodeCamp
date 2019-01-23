6fcbfe55def093436abdab0e5035c5598563c737
Monkey <duyk16@gmail.com>
2019-01-23 16:24:06 +0700

    upload api

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
-app.get('/api/:time', (req, res) => {
-
+app.get('/api/timestamp/:dateString', (req, res) => {
+  let dateString = req.params.dateString
+  let date = new Date();
+  if (dateString) {
+    dateString = dateString == parseInt(dateString) ? parseInt(dateString) : dateString
+    date = new Date(dateString);
+  } 
+  if (date == "Invalid Date") {
+    return res.status(300).send({
+      error: "Invalid Date"
+    })
+  }
+  return res.status(200).send({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
 })
 
 app.listen(port, () => {console.log(`Listening on port ${port}...`)})
