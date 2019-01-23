e2f584c796259a617f9c820c722ecddde48e36da
Monkey <duyk16@gmail.com>
2019-01-23 17:05:39 +0700

    fix emty parms

--- routes/TimeStamp.js
+++ routes/TimeStamp.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const route = express.Router()
 
+
+route.get('/', (req, res) => {
+  let date = new Date();
+  return res.status(200).send({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
 route.get('/:dateString', (req, res) => {
   let dateString = req.params.dateString
-  let date = new Date();
-  if (dateString) {
-    dateString = dateString == parseInt(dateString) ? parseInt(dateString) : dateString
-    date = new Date(dateString);
-  }
+  dateString = dateString == parseInt(dateString) ? parseInt(dateString) : dateString
+  date = new Date(dateString);
   if (date == "Invalid Date") {
     return res.status(300).send({
       error: "Invalid Date"
